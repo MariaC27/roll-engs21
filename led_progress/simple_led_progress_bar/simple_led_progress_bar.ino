c9ab83bb38cd7aff050516379bcca4d04ec292e6
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2023-05-14 21:37:29 -0400

    add buzzer code

---
+++ led_progress/simple_led_progress_bar/simple_led_progress_bar.ino
@@ -0,0 +1,67 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// Released under the GPLv3 license to match the rest of the
+// Adafruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        0 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 12 // Popular NeoPixel ring size
+
+int pinBuzzer = 3;// the onboard buzzer on the XIAO expansion board is A3
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 5000 // Time (in milliseconds) to pause between pixels - 5 seconds for 12 pixels = 1 minute for roller circle
+
+void setup() {
+  pinMode(pinBuzzer, OUTPUT);// initialize the buzzer pin as an output
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+}
+
+void loop() {
+  pixels.clear(); // Set all pixel colors to 'off'
+
+  // The first NeoPixel in a strand is #0, second is 1, all the way up
+  // to the count of pixels minus one.
+  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+
+    pixels.show();   // Send the updated pixel colors to the hardware.
+
+    tone(pinBuzzer, 200);
+    delay(100); // plays a 1-second tone when pixels show
+    noTone(pinBuzzer);
+    
+    delay(DELAYVAL); // Pause before next pass through loop
+  }
+  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+
+    pixels.show();   // Send the updated pixel colors to the hardware.
+  }
+  delay(15000);
+
+}
--- roller/roller.ino
+++ roller/roller.ino
@@ -1,37 +1,37 @@
 
-#define LED_1 13
-#define LED_2 11
+#define LED_1 11
+#define LED_2 10
 #define LED_3 9
 #define LED_4 8
 
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
+//Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
 
 void setup() { //runs once
-  Serial.begin(9600);  
-   if(!accel.begin())
-   {
-      Serial.println("ADXL345 sensor not detected.");
-      while(1);
-   }
+  // Serial.begin(9600);  
+  //  if(!accel.begin())
+  //  {
+  //     Serial.println("ADXL345 sensor not detected.");
+  //     while(1);
+  //  }
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
   pinMode(LED_3, OUTPUT);
   pinMode(LED_4, OUTPUT);
   delay(2000);
-  analogWrite(LED_1, 255);
+  digitalWrite(LED_1, HIGH);
   delay(2000);
-  analogWrite(LED_2, 255);
+  digitalWrite(LED_2, HIGH);
   delay(2000);
-  analogWrite(LED_3, 255);
+  digitalWrite(LED_3, HIGH);
   delay(2000);
-  analogWrite(LED_4, 255);
+  digitalWrite(LED_4, HIGH);
 
 }
 
 void loop() { //runs repeatedly
-   sensors_event_t event; 
-   accel.getEvent(&event);
-   Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-   Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-   Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");
+  //  sensors_event_t event; 
+  //  accel.getEvent(&event);
+  //  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  //  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  //  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");
 }
